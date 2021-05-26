d4fb1ddec26d2bad91d3573d3e3dc5e24412c2af
semi_cloud <sksmsrkdtpal@naver.com>
2021-05-26 16:04:07 +0900

    add schema

---
+++ Campers/schema/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const camperSchema = new Schema({
+    User: {
+        type:
+        required:
+        unique
+
+      
+    },
+})
