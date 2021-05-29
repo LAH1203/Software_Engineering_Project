b97b9138cc45d45cc77414606d5312f875d3ab39
LAH1203 <lah1203@naver.com>
2021-05-29 16:38:37 +0900

    Modify signup.js

--- Campers/public/signup.js
+++ Campers/public/signup.js
@@ -1,52 +1,28 @@
-function checkfield(){
+function signupform_check() {
 
-    if(document.sign_form.name.value==""){
-      alert("이름을 입력하세요");
-      document.addjoin.name.focus();
-      exit;
-      
-    }else if(document.sign_form.email.value==""){
-      alert("이메일을 입력하세요");
-      document.addjoin.email.focus();
-      exit;
-      
-    }else if(document.sign_form.password.value==""){
-      alert("비밀번호를 입력하세요");
-      document.addjoin.pw.focus();
-      exit;
+  var reg = /^[0-9]+/g; //숫자만 입력하는 정규식
+
+  if (document.sign_form.name.value == "") {
+    alert("이름을 입력하세요.");
+    document.sign_form.name.value.focus();
+    return false;
+  } else if (document.sign_form.email.value == "") {
+    alert("이메일 주소를 입력하세요.");
+    document.sign_form.email.value.focus();
+    return false;
+  } else if (document.sign_form.password.value == "") {
+    alert("비밀번호를 입력하세요.");
+    document.sign_form.password.value.focus();
+    return false;
+  } else if (document.sign_form.password.value !== document.sign_form.password2.value) {
+    alert("비밀번호가 일치하지 않습니다.");
+    document.sign_form.password2.value.focus();
+    return false;
+  }else if (!reg.test(document.sign_form.phone.value)) {
+    alert("전화번호는 숫자만 입력할 수 있습니다.");
+    document.sign_form.phone.value.focus();
+    return false;
+  }
     
-    }else if(document.sign_form.password2.value==""){
-      alert("비밀번호확인을 입력하세요");
-      document.addjoin.pw2.focus();
-      exit;
-    
-    }else if(document.sign_form.phone.value == ""){
-      alert("전화번호를 입력하세요");
-      document.sign_form.focus();
-      exit;
-    }
-    
-    if(document.sign_form.password.value!=document.sign_form.password2.value){
-    //비밀번호와 비밀번호확인의 값이 다를 경우
-    
-    alert("입력한 2개의 비밀번호가 일치하지 않습니다.");
-    document.sign_form.password.focus();
-    exit;
-    
-    }
-    
-    var exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-    
-    if(exptext.test(document.sign_form.email.value)==false){
-    //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우
-    
-    alert("이 메일형식이 올바르지 않습니다.");
-    document.sign_form.email.focus();
-    exit;
-    }
-  
-    document.sign_form.email.submit();
-    
-   }
-  
-  
+  document.sign_form.submit();
+}
--- Campers/server.js
+++ Campers/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
+var msg = require('dialog');
+var async = require('async');
 var mongoose = require('mongoose');
 const crypto = require('crypto');
 const User = require('./schema/User');
@@ -130,17 +132,19 @@ app.post('/login', function(req, res) {
 
 // 회원가입 화면
 app.get('/signup', function(req, res) {
-
+    res.render('signup_page');
 });
 
 app.post('/signup', function(req, res) {
-
+    console.log('회원가입 성공');
+    res.redirect('/login');
 });
 
 // 마이페이지
 app.get('/mypage', function(req, res) {
     // 세션에 사용자 정보가 저장되어있지 않을 경우 로그인 하지 않은 상태이므로 로그인을 먼저 하도록 함
-    if (!req.session.email) {
+    
+    if (!req.session.name) {
         res.redirect('/login');
     }
     var userInfo = {};
@@ -148,39 +152,104 @@ app.get('/mypage', function(req, res) {
     userInfo.email = req.session.email;
     userInfo.phoneNumber = req.session.phoneNumber;
     userInfo.mode = req.session.mode;
-    var sortByDate = { Start_date : -1 };
+    
+    /*
+    var userInfo = {};
+    userInfo.name = "바보";
+    userInfo.email = "hahaha@email.com";
+    userInfo.phoneNumber = "01011111111";
+    userInfo.mode = 1;
+    */
 
-    let _id = [];
-    let campgroundName = [];
-    let startDate = [];
-    let endDate = [];
-    let approvalDate = [];
-    let checkinDate = [];
+    // 고객
+    if (userInfo.mode == 0) {
+        var sortByDate = { Start_date : -1 };
 
-    Reservation.find({Reservation_email: new RegExp(`${userInfo.email}`)})
-        .sort(sortByDate)
-        .then((result) => {
-            for(var i = 0; i < result.length ; i++) {
-                _id.push(result[i]._id);
-                campgroundName.push(result[i].Campground_name);
-                startDate.push(result[i].Start_date);
-                endDate.push(result[i].End_date);
-                approvalDate.push(result[i].Approval_date);
-                checkinDate.push(result[i].Checkin_date);
-            }
-            res.render('MyPage', {
-                userInfo: userInfo,
-                _id: _id,
-                campgroundName: campgroundName,
-                startDate: startDate,
-                endDate: endDate,
-                approvalDate: approvalDate,
-                checkinDate: checkinDate
+        let _id = [];
+        let campgroundName = [];
+        let startDate = [];
+        let endDate = [];
+        let approvalDate = [];
+        let checkinDate = [];
+
+        Reservation.find({Reservation_email: new RegExp(`${userInfo.email}`)})
+            .sort(sortByDate)
+            .then((result) => {
+                for (var i = 0; i < result.length ; i++) {
+                    _id.push(result[i]._id);
+                    campgroundName.push(result[i].Campground_name);
+                    startDate.push(result[i].Start_date);
+                    endDate.push(result[i].End_date);
+                    approvalDate.push(result[i].Approval_date);
+                    checkinDate.push(result[i].Checkin_date);
+                }
+                res.render('MyPage', {
+                    userInfo: userInfo,
+                    _id: _id,
+                    campgroundName: campgroundName,
+                    startDate: startDate,
+                    endDate: endDate,
+                    approvalDate: approvalDate,
+                    checkinDate: checkinDate
+                });
+            })
+            .catch((err) => {
+                console.log(err);
             });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    }
+    // 캠핑장 주인
+    else if (userInfo.mode == 1) {
+        var mysort = { Campground_name : -1 };
+
+        let camp_id = [];
+        let camp_name = [];
+        let camp_location = [];
+        let reservation_email = [];
+        let start_date = [];
+        let end_date = [];
+        let number_of_people = [];
+
+        Campground.find({Owner_email: new RegExp(`${userInfo.email}`)})
+            .sort(mysort)
+            .then((result) => {
+                for (var i = 0; i < result.length ; i++) { 
+                    camp_name.push(result[i].Campground_name) ;
+                    camp_id.push(result[i].Campground_id);
+                    camp_location.push(result[i].Campground_location);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        
+        for (var i = 0; i < camp_name.length; i++) {
+            Reservation.find({Campground_name: `${camp_name[i]}`})
+                .then((result) => {
+                    reservation_email.push(result[0].Reservation_email);
+                    start_date.push(result[0].Start_date);
+                    end_date.push(result[0].End_date);
+                    number_of_people.push(result[0].number_of_people);
+                    res.render('mypage_owner', {
+                        userInfo: userInfo,
+                        camp_name: camp_name,
+                        camp_id: camp_id,
+                        camp_location: camp_location,
+                        reservation_email: reservation_email,
+                        start_date: start_date,
+                        end_date: end_date,
+                        number_of_people: number_of_people
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+    // mode가 2면 관리자
+    else if (userInfo.mode == 2) {
+        msg.info('관리자는 마이페이지가 존재하지 않습니다.');
+        res.redirect('/');
+    }
 });
 
 // 후기 작성 및 수정 화면
