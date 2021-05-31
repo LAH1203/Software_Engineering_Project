c4cdc7e2ab0b23e7df4b8cd27b2ae1550456fea1
seojung <seojungyoo22@gamil.com>
2021-06-01 00:49:09 +0900

    질문작성 버튼

--- Campers/public/campground_main.js
+++ Campers/public/campground_main.js
@@ -1,11 +1,35 @@
+//Q&A 작성화면으로 가는 함수
+function goTowamQnAPage(id) {
+    var url = 'http://localhost:3000/setqna';
+    var queryParams = '?id=' + id;
+    var link = url + queryParams;
+    location.href = link;
+}
+
 function setInformation(info) {
     let x = document.getElementsByClassName("quiz-text")[0];
     x.innerText=info; 
 }
-function setQnA(qna) {
+
+function setQnA(id, qna) {
     let x = document.getElementsByClassName("quiz-text")[0];
-    x.innerText=qna; 
+    x.innerText='';
+    var btn = document.createElement("button");
+    btn.innerText = "질문작성";
+    btn.onclick = function() {
+        var url = 'http://localhost:3000/setqna';
+        var queryParams = '?id=' + id;
+        var link = url + queryParams;
+        location.href = link;
+    }
+    //btn.setAttribute("onclick","goTowamQnAPage(id)");
+    x.appendChild(btn);
+    var qnatext = document.createElement("text");
+    qnatext.innerText = qna;
+    x.appendChild(qnatext);
+    //x.innerText += input(type="button", value='작성하기', onclick="goTowamQnAPage(pageid)");
 }
+
 function setReview(rev) {
     let x = document.getElementsByClassName("quiz-text")[0];
     x.innerText=rev;
